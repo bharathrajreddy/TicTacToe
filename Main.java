6068176b3130988e0ad33872005f90c6a0954f41
bharath <bharathkumarreddyks@gmail.com>
2023-06-25 21:03:32 +0530

    Initial commit of TicTacToe

---
+++ Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        TicTacToe tictactoe = new TicTacToe();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter player 1 name ");
+        String X = sc.nextLine();
+        System.out.println("Enter player 1 name ");
+        String O = sc.nextLine();
+
+    }
+}
